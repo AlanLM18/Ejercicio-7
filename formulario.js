ed049a5b0455d393e11ef451b17e85e44aa1bdf7
AlanLM18 <alanlm.ti23@utsjr.edu.mx>
2024-06-26 07:43:21 -0600

    first commit

---
+++ formulario.js
@@ -0,0 +1,11 @@
+console.log("Hola");
+var nombre="Fulanito";
+var edad= 20;
+var estatura= 1.50;
+
+function anyosFaltantes(edad){
+    var anyosFaltantes= 100-edad;
+    return anyosFaltantes
+}
+
+console.log(anyosFaltantes(edad));
---
+++ formulario1.js
@@ -0,0 +1,6 @@
+var nombre=document.getElementById("nombre");
+nombre.addEventListener("blur",recibeNombre);
+
+function recibeNombre(){
+    console.log("hola");
+}
